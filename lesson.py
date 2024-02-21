7e4355634c22ddd31aeb8248db9a3feeecaf9816
wersomd <ileskazybek@gmail.com>
2024-02-21 21:35:30 +0600

    First Push

---
+++ lesson.py
@@ -0,0 +1,4 @@
+user_name = 'Farzali'
+
+
+print(f'Hello World {user_name}')
---
+++ lib/core/app_style.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+const h1Style = TextStyle(
+  fontSize: 48,
+  color: Colors.black,
+  fontFamily: "Rubik",
+  height: 1.2,
+  fontWeight: FontWeight.w900,
+);
+
+const h2Style = TextStyle(
+  fontSize: 22,
+  color: Colors.black,
+  fontFamily: "Rubik",
+  fontWeight: FontWeight.w600,
+);
+
+const h3Style = TextStyle(
+  fontFamily: "Rubik",
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+const h4Style = TextStyle(
+  fontFamily: "Rubik",
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+const h5Style = TextStyle(
+  fontFamily: "Rubik",
+  fontSize: 20,
+  fontWeight: FontWeight.w300,
+  color: Colors.black,
+);
+
+const bodyStyle1 = TextStyle(
+  fontFamily: "Rubik",
+  fontSize: 18,
+  fontWeight: FontWeight.w100,
+  color: Colors.black,
+);
+
+final textFieldStyle = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(15),
+  borderSide: const BorderSide(color: Colors.grey, width: 1.0),
+);
